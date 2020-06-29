65c0d41820446bf166ec3c20551442a7e3bdf52d
Apriani magfira <57305690+13020180058@users.noreply.github.com>
2020-06-29 08:03:15 +0800

    Add files via upload

---
+++ UAS_13020180058/nbproject/Penjualan.java
@@ -0,0 +1,17 @@
+
+package penjualan;
+
+/**
+ *
+ * @author Hp
+ */
+public class Penjualan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
